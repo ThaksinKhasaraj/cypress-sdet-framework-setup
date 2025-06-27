9a537f128d8e4bf8a5e6ae26dae6f4388af05cc2
SinSaraj <73773033+ThaksinKhasaraj@users.noreply.github.com>
2025-06-28 03:52:24 +0700

    Import custom commands in e2e.js

---
+++ cypress/support/e2e.js
@@ -0,0 +1,4 @@
+// This file is processed and loaded automatically before your test files.
+// You can use this file to import custom commands, set up global hooks, or configure Cypress.
+
+import './commands';
